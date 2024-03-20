def59cb72de707083f229baef39eccbf79a0569a
Sanjit Raman <51057192+SanjitRaman@users.noreply.github.com>
2024-03-20 18:49:46 +0000

    Prefix implemented. A careful check would be great from @Cheesypasta1

--- src/ast/operators/ast_prefix.cpp
+++ src/ast/operators/ast_prefix.cpp
@@ -3,6 +3,23 @@
 void PrefixOperator::EmitRISC(std::ostream &stream, Context &context,
                               std::string destReg) const
 {
+    // todo support other types such as float, double, char, unsigned
+    std::string tempReg = context.allocateReg(stream);
+    primary_expression_->EmitRISC(stream, context, tempReg);
+    if (op_ == "++")
+    {
+        stream << "addi " << tempReg << ", " << tempReg << ", 1" << std::endl;
+    }
+    else if (op_ == "--")
+    {
+        // Implement decrement
+        stream << "addi " << tempReg << ", " << tempReg << ", -1" << std::endl;
+    }
+    stream << "sw " << tempReg << ", "
+           << context.getOffset(primary_expression_->getId()) << "(sp)"
+           << std::endl;
+    stream << "mv " << destReg << ", " << tempReg << std::endl;
+    context.deallocateReg(tempReg);
 }
 
 void PrefixOperator::EmitRISC(std::ostream &stream, Context &context) const
@@ -36,5 +53,6 @@ void PrefixOperator::Print(std::ostream &stream) const
 
 data_type PrefixOperator::getType(Context &context) const
 {
+    // TODO: change for different types.
     return data_type::_int;
 }
